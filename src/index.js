388220a8ca36eab98b20f5b7ab1b6ad69aac118c
akira-cn <akira.cn@gmail.com>
2018-03-26 18:24:47 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-import {Color, parseColor} from 'sprite-utils'
 import BaseSprite from './basesprite'
 import Sprite from './sprite'
 import Label from './label'
 import Layer from './layer'
-import {Matrix, Vector} from 'sprite-math'
 import Group from './group'
-import {Effects} from 'sprite-animator'
 
 export {
   BaseSprite,
   Sprite,
   Label,
   Layer,
-  Color,
-  parseColor,
-  Effects,
-  Matrix,
   Group,
-  Vector,
 }
