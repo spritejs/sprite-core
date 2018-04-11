b8b52a731a07702c9c6ffd50bfd8b31a7b63d5a1
akira-cn <akira.cn@gmail.com>
2018-04-11 11:53:42 +0800

    :bug:fixed basesprite translate x, y reversed bug

--- src/basesprite.js
+++ src/basesprite.js
@@ -461,7 +461,7 @@ export default class BaseSprite extends BaseNode {
 
     drawingContext.restore()
 
-    drawingContext.translate(borderWidth + attr.padding[0], borderWidth + attr.padding[3])
+    drawingContext.translate(borderWidth + attr.padding[3], borderWidth + attr.padding[0])
 
     return drawingContext
   }
